2544b0e1713b2057c9317e479f2820d66ea62114
smaru1111 <marusou20@gmail.com>
2023-07-20 19:07:10 +0900

    #3 update fetch

--- hum_a/hum_a.ino
+++ hum_a/hum_a.ino
@@ -1,11 +1,13 @@
 const int ledPin = 4;
 const int pirPin = 2;
+
  
 void setup() {
   // put your setup code here, to run once:
   // シリアル通信速度
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
+  delay(5000);
 }
  
 void loop() {
@@ -13,9 +15,12 @@ void loop() {
   if (digitalRead(pirPin) == LOW) {
     digitalWrite(ledPin, HIGH);
     Serial.write(1);
+    Serial.println(1);         // シリアル通信でカウンターの値をPCに送信
     delay(100);
   } else {
     digitalWrite(ledPin, LOW);
     Serial.write(0);
+    Serial.println(0);         // シリアル通信でカウンターの値をPCに送信
+    delay(100);
   }
 }
